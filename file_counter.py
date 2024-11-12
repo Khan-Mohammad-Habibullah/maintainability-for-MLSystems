0aefedf0642aa14fdf2eefee0c46ae962b182e81
Khan-Mohammad-Habibullah <61003413+Khan-Mohammad-Habibullah@users.noreply.github.com>
2024-11-12 20:20:55 +0100

    file counter and ML components partitions uploaded

---
+++ file_counter.py
@@ -0,0 +1,51 @@
+import ML_components
+print (len(ML_components.face_recognition_Data_Aquisition))
+print (len(ML_components.face_recognition_Training_Pipeline))
+print (len(ML_components.face_recognition_ML_interfacing))
+print (len(ML_components.face_recognition_Non_ML))
+
+print (len(ML_components.faceswap_Data_Aquisition))
+print (len(ML_components.faceswap_Training_Pipeline))
+print (len(ML_components.faceswap_ML_interfacing))
+print (len(ML_components.faceswap_Non_ML))
+
+print (len(ML_components.open_assistant_Data_Aquisition))
+print (len(ML_components.open_assistant_Training_Pipeline))
+print (len(ML_components.open_assistant_ML_interfacing))
+print (len(ML_components.open_assistant_Non_ML))
+
+print (len(ML_components.deepfacelive_Data_Aquisition))
+print (len(ML_components.deepfacelive_Training_Pipeline))
+print (len(ML_components.deepfacelive_ML_interfacing))
+print (len(ML_components.deepfacelive_Non_ML))
+
+print (len(ML_components.CLIP_Data_Aquisition))
+print (len(ML_components.CLIP_Training_Pipeline))
+print (len(ML_components.CLIP_ML_interfacing))
+print (len(ML_components.CLIP_Non_ML))
+
+print (len(ML_components.easyOCR_Data_Aquisition))
+print (len(ML_components.easyOCR_Training_Pipeline))
+print (len(ML_components.easyOCR_ML_interfacing))
+print (len(ML_components.easyOCR_Non_ML))
+
+
+print (len(ML_components.DocsGPT_Data_Aquisition))
+print (len(ML_components.DocsGPT_Training_Pipeline))
+print (len(ML_components.DocsGPT_ML_interfacing))
+print (len(ML_components.DocsGPT_Non_ML))
+
+print (len(ML_components.Chatterbot_Data_Aquisition))
+print (len(ML_components.Chatterbot_Training_Pipeline))
+print (len(ML_components.Chatterbot_ML_interfacing))
+print (len(ML_components.Chatterbot_Non_ML))
+
+print (len(ML_components.deepface_Data_Aquisition))
+print (len(ML_components.deepface_Training_Pipeline))
+print (len(ML_components.deepface_ML_interfacing))
+print (len(ML_components.deepface_Non_ML))
+
+print (len(ML_components.latexocr_DA))
+print (len(ML_components.latexocr_TP))
+print (len(ML_components.latexocr_ML))
+print (len(ML_components.latexocr_NON))
